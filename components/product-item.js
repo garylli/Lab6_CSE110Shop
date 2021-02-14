324bcfde000dc07226198c8400c695c2276970a9
Gary <gll001@ucsd.edu>
2021-02-13 23:48:54 -0800

    Working 1-4

--- components/product-item.js
+++ components/product-item.js
@@ -1,7 +1,53 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
-  // TODO
-}
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
 
+    const card = document.createElement('li');
+    card.setAttribute('class', 'product');
+
+    const img = card.appendChild(document.createElement('img'));
+    img.src = this.hasAttribute('img') ? this.getAttribute('img') : '/noimg.png';
+    img.setAttribute('alt', this.getAttribute('title-text'));
+
+    const productTitle = card.appendChild(document.createElement('p'));
+    productTitle.setAttribute('class','title-text');
+    productTitle.textContent = this.getAttribute('title-text');
+
+    const productPrice = card.appendChild(document.createElement('p'));
+    productPrice.setAttribute('class', 'price');
+    productPrice.textContent = this.getAttribute("price-num")
+
+    const addButton = card.appendChild(document.createElement('button'));
+    addButton.setAttribute('onclick', "alert('Added to Cart!')")
+    addButton.textContent = "Add to Cart";
+
+    /* css styling */
+    const styleLink = document.createElement('link');
+    styleLink.setAttribute('rel', 'stylesheet');
+    styleLink.setAttribute('href','./styles/styles.css');
+
+    this.shadowRoot.append(card,styleLink);
+  }
+
+  connectedCallback() {
+    console.log('product added to page.');
+    updateAttributes(this);
+  }
+}
 customElements.define('product-item', ProductItem);
+function updateAttributes(elem) {
+  const shadow = elem.shadowRoot;
+  
+  const img = shadow.querySelector("img");
+    img.src = elem.hasAttribute('img') ? elem.getAttribute('img') : '/noimg.png';
+    img.setAttribute('alt', elem.getAttribute('title-text'));
+  const productTitle = shadow.querySelector(".title-text");
+    productTitle.setAttribute('class','title-text');
+    productTitle.textContent = elem.getAttribute('title-text');
+  const productPrice = shadow.querySelector(".price");
+    productPrice.setAttribute('class', 'price');
+    productPrice.textContent = elem.getAttribute("price-num")
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,28 @@
 // Script.js
+let localStorage = window.localStorage;
 
-window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+window.addEventListener('DOMContentLoaded', () => { 
+  fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => storeData(JSON.stringify(data)))
+    .then(createCards(localStorage.getItem('fakestoredata')));
 });
+
+function storeData(data) {
+  if (localStorage.getItem('fakestoredata') == null) {
+    localStorage.setItem('fakestoredata',data);
+  }
+}
+
+function createCards(data) {
+  const items = JSON.parse(data);
+  let parent = document.querySelector(".flex-container");
+  console.log(parent);
+  items.forEach(element => {
+    const item = document.createElement('product-item');
+    item.setAttribute('img', element.image);
+    item.setAttribute('title-text', element.title)
+    item.setAttribute('price-num', element.price);
+    parent.appendChild(item);
+  });
+}

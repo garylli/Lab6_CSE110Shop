5a9eff676ed3bfa3ac68b59d477aee3c3bc227e6
Gary <gll001@ucsd.edu>
2021-02-14 01:08:37 -0800

    fixed error with first load

--- scripts/script.js
+++ scripts/script.js
@@ -2,25 +2,26 @@
 let localStorage = window.localStorage;
 
 window.addEventListener('DOMContentLoaded', () => { 
-  fetch('https://fakestoreapi.com/products')
-    .then(response => response.json())
-    .then(data => storeData(JSON.stringify(data)))
-    .then(createCards(localStorage.getItem('fakestoredata')));
+  if (localStorage.getItem('fakestoredata') == null){
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => createCards(JSON.stringify(data)))
+  } else {
+    createCards(localStorage.getItem('fakestoredata'));
+  }
 });
 
-function storeData(data) {
+
+function createCards(data) {
   if (localStorage.getItem('fakestoredata') == null) {
     localStorage.setItem('fakestoredata',data);
   }
-}
-
-function createCards(data) {
   const items = JSON.parse(data);
   let parent = document.querySelector(".flex-container");
   items.forEach(element => {
     const item = document.createElement('product-item');
     item.setAttribute('img', element.image);
-    item.setAttribute('title-text', element.title)
+    item.setAttribute('title-text', element.title);
     item.setAttribute('price-num', element.price);
     item.setAttribute('id', element.id);
     parent.appendChild(item);
